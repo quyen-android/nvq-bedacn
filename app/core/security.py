ea7f22cba1b1b93244a17cafad177f40a16f604c
quyen-android <vanquyenk22ute@gmail.com>
2026-04-12 10:38:17 +0700

    authorization

--- app/core/security.py
+++ app/core/security.py
@@ -10,39 +10,3 @@ def hash_password(password: str):
 
 def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed)
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
-
-    to_encode.update({
-        "exp": expire,
-        "type": "access"
-    })
-
-    return jwt.encode(
-        to_encode,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
-
-def create_refresh_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(
-        days=settings.REFRESH_TOKEN_EXPIRE_DAYS
-    )
-
-    to_encode.update({
-        "exp": expire,
-        "type": "refresh"
-    })
-
-    return jwt.encode(
-        to_encode,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
--- app/repositories/token_repo.py
+++ app/repositories/token_repo.py
@@ -1,5 +1,6 @@
 from app.models.refresh_token import RefreshToken
 from datetime import datetime
+from sqlalchemy.orm import Session
 
 class TokenRepository:
     def create(self, db, ma_nguoi_dung, ma_token, thoi_gian_het_han):
@@ -22,3 +23,12 @@ class TokenRepository:
         if rt:
             rt.da_thu_hoi = True
             db.commit()
+
+    def revoke_all_by_user(self, db: Session, user_id: int):
+        db.query(RefreshToken).filter(
+            RefreshToken.ma_nguoi_dung == user_id
+        ).update(
+            {"da_thu_hoi": True},
+            synchronize_session=False
+        )
+        db.commit()
